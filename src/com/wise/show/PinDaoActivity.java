d0cb554a5a981372e6201b15f3e5d839a9cef288
wang <wang@wang-PC>
2014-10-09 11:21:46 +0800

    ghdfgdgh
    Signed-off-by: wang <wang@wang-PC>

--- src/com/wise/show/PinDaoActivity.java
+++ src/com/wise/show/PinDaoActivity.java
@@ -4,13 +4,25 @@ import com.wise.baba.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
 
 public class PinDaoActivity extends Activity {
-	
+	ListView pindaoListView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pindao);
+		pindaoListView = (ListView) findViewById(R.id.list_pindao);
+
+		findViewById(R.id.iv_back).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 
 }
