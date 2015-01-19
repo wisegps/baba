f1546a8cb2517bf5ad64f129cebf4121755e9109
honesty <371278698@qq.com>
2015-01-19 09:35:22 +0800

    Signed-off-by: honesty <371278698@qq.com>

--- src/com/wise/state/ManageActivity.java
+++ src/com/wise/state/ManageActivity.java
@@ -151,7 +151,12 @@ public class ManageActivity extends Activity {
 
 		@Override
 		public int getChildrenCount(int groupPosition) {
-			return (carDatas.size() == 0)?0:1;
+			List<String> strs = details.get(groupPosition);
+			if(strs.size() == 0){
+				return 0;
+			}else{
+				return 1;
+			}
 		}
 
 		@Override
@@ -343,5 +348,11 @@ public class ManageActivity extends Activity {
 		private class ViewHolder {
 			TextView tv_item_hot;
 		}
+	}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		//static 文件手动删除
+		carDatas.clear();
 	}
 }
