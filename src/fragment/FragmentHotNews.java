edd0e43721a392bad848c80056d88e12d9e5c6f7
c <c@c-PC>
2015-04-22 15:37:30 +0800

    优化

--- src/fragment/FragmentHotNews.java
+++ src/fragment/FragmentHotNews.java
@@ -2,6 +2,8 @@ package fragment;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import listener.OnCardMenuListener;
 import android.content.Intent;
@@ -55,7 +57,7 @@ public class FragmentHotNews extends Fragment implements Callback {
 		tvContent = (TextView) getActivity().findViewById(
 				R.id.tv_title0_content);
 		llytNews = (LinearLayout) getActivity().findViewById(R.id.llytNews);
-		
+
 		ImageView iv_weather_menu = (ImageView) getActivity().findViewById(
 				R.id.iv_hot_news_menu);
 		llytNews.setOnClickListener(onClickListener);
@@ -80,15 +82,15 @@ public class FragmentHotNews extends Fragment implements Callback {
 				break;
 			case R.id.tv_title1:
 				index = 1;
-				break; 
+				break;
 			case R.id.tv_title2:
 				index = 2;
-				break; 
+				break;
 			case R.id.tv_title3:
 				index = 3;
-				break; 
+				break;
 			}
-			if(index!=-1){
+			if (index != -1) {
 				Intent intent_hot = new Intent(getActivity(),
 						BrowserActivity.class);
 				News news = newsList.get(index);
@@ -97,7 +99,7 @@ public class FragmentHotNews extends Fragment implements Callback {
 				intent_hot.putExtra("hot_content", news.getContent());
 				startActivity(intent_hot);
 			}
-			
+
 		}
 	};
 
@@ -114,7 +116,7 @@ public class FragmentHotNews extends Fragment implements Callback {
 	 */
 	@Override
 	public boolean handleMessage(Message msg) {
-		switch(msg.what){
+		switch (msg.what) {
 		case Msg.Get_News_List:
 			this.newsList = (List<News>) msg.obj;
 			setNewsTitle();
@@ -122,32 +124,38 @@ public class FragmentHotNews extends Fragment implements Callback {
 		}
 		return false;
 	}
-	
-	
+
 	/**
 	 * 设置首页新闻
 	 */
-	public void setNewsTitle(){
-		if(newsList== null){
+	public void setNewsTitle() {
+		if (newsList == null) {
 			return;
 		}
-		
-		
+
 		int titleLength = titleId.length;
 		int newsSize = newsList.size();
-		int count = titleLength<newsSize?titleLength:newsSize;
-		Log.i("FragmentHotNews", "count "+count);
-		for(int i =0;i<count;i++){
+		int count = titleLength < newsSize ? titleLength : newsSize;
+		Log.i("FragmentHotNews", "count " + count);
+		for (int i = 0; i < count; i++) {
 			News news = (News) newsList.get(i);
-			if(i == 0 ){
-				String content =news.getContent().trim().replaceAll("/n", "");
-				Log.i("FragmentHotNews", "getContent "+content);
+			if (i == 0) {
+				String content = news.getContent().replaceAll("】", "】 ");
+				Log.i("FragmentHotNews", "getContent " + content);
 				tvContent.setText(content);
 			}
-			TextView tvTitle = (TextView) getActivity().findViewById(titleId[i]);
+			TextView tvTitle = (TextView) getActivity()
+					.findViewById(titleId[i]);
 			tvTitle.setOnClickListener(onClickListener);
 			tvTitle.setText(news.getTitle());
 		}
+	}
+
+	// 在标点符号后加一个空格。
+	public static String stringFilter(String str) {
+		str = str.replaceAll("】", "】 ").replaceAll("，", "， ")
+				.replaceAll("。", "。 ");// 替换中文标号
+		return str.trim();
 	}
 
 	@Override
@@ -156,6 +164,5 @@ public class FragmentHotNews extends Fragment implements Callback {
 		super.onDestroy();
 		http.cancle();
 	}
-	
-	
+
 }
