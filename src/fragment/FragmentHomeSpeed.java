9193a8738be4d9c93d41781dcbb6e96ab6b31264
c <c@c-PC>
2015-04-23 14:24:03 +0800

    速度优化

--- src/fragment/FragmentHomeSpeed.java
+++ src/fragment/FragmentHomeSpeed.java
@@ -1,10 +1,5 @@
 package fragment;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-import pubclas.Constant;
-import pubclas.NetThread;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Handler.Callback;
@@ -16,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.wise.baba.AppApplication;
@@ -23,7 +19,6 @@ import com.wise.baba.R;
 import com.wise.baba.app.Msg;
 import com.wise.baba.biz.HttpGetObdData;
 import com.wise.baba.ui.widget.ClockwiseDialView;
-import com.wise.baba.ui.widget.DialView;
 
 /**
  * 
@@ -38,17 +33,25 @@ public class FragmentHomeSpeed extends Fragment implements Callback,
 	private HttpGetObdData http;
 	private ClockwiseDialView dialSpeed;
 	/**
-	 * 0，速度 1，转速 2，电源电压 3，水温 4 ，负荷 5，节气门  ，6，剩余油量
+	 * 0，速度 1，转速 2，电源电压 3，水温 4 ，负荷 5，节气门 ，6，剩余油量
 	 */
 	private View view;
 	private int textId[] = { R.id.textSpeed, R.id.textRotary, R.id.textVoltage,
 			R.id.textTemperature, R.id.textLoad, R.id.textThrottle,
 			R.id.textOil };
+
+	private int icon[] = { R.drawable.ico_speed,
+			R.drawable.ico_home_speed__rotary,
+			R.drawable.ico_home_speed_voltage,
+			R.drawable.ico_home_speed__temperature,
+			R.drawable.ico_home_speed__load,
+			R.drawable.ico_home_speed_throttle, R.drawable.ico_home_speed_oil };
 	private int llytId[] = { R.id.llytSpeed, R.id.llytRotary, R.id.llytVoltage,
 			R.id.llytTemperature, R.id.llytLoad, R.id.llytThrottle,
 			R.id.llytOil };
 	private int value[] = new int[7];
-	private TextView textScore,textUnit;
+	private TextView tvCardTitle, textScore, textUnit;
+	private ImageView ivCardIcon;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -59,9 +62,10 @@ public class FragmentHomeSpeed extends Fragment implements Callback,
 		for (int i = 0; i < 7; i++) {
 			view.findViewById(llytId[i]).setOnClickListener(this);
 		}
-
+		tvCardTitle = (TextView) view.findViewById(R.id.tv_card_title);
 		textScore = (TextView) view.findViewById(R.id.tv_score);
-		textUnit= (TextView) view.findViewById(R.id.tv_unit);
+		textUnit = (TextView) view.findViewById(R.id.tv_unit);
+		ivCardIcon = (ImageView) view.findViewById(R.id.iv_card_icon);
 		return view;
 	}
 
@@ -76,11 +80,8 @@ public class FragmentHomeSpeed extends Fragment implements Callback,
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
 		app = (AppApplication) this.getActivity().getApplication();
-		
-		
-		
-	}
 
+	}
 
 	@Override
 	public void onResume() {
@@ -114,11 +115,11 @@ public class FragmentHomeSpeed extends Fragment implements Callback,
 						.setText(value[i] + "");
 			}
 			textScore.setText(value[0] + "");
-			dialSpeed.initValue(value[0],handler);
-		}else if(msg.what == Msg.Dial_Refresh_Value){
+			dialSpeed.initValue(value[0], handler);
+		} else if (msg.what == Msg.Dial_Refresh_Value) {
 			int value = msg.arg1;
 			textScore.setText(value + "");
-			//dialSpeed.startCheckAnimation(value, handler);
+			// dialSpeed.startCheckAnimation(value, handler);
 		}
 
 		return false;
@@ -133,26 +134,52 @@ public class FragmentHomeSpeed extends Fragment implements Callback,
 	public void onClick(View v) {
 
 		int id = v.getId();
-		for (int i = 0; i < 7; i++) {
-			if (id == llytId[i]) {
-				//先设置单位
-				if(i<1){
-					textUnit.setText("km");
-				}else if(i<1){
-					textUnit.setText("rpm");
-				} else if(i == 2){
-					textUnit.setText("v");
-				}else if(i == 3){
-					textUnit.setText("℃");
-				}else{
-					textUnit.setText("%");
-				}
-				
-				Log.i("FragmentHomeSpeed", "clicke" + i);
-				dialSpeed.startAnimation(value[i], handler);
-				break;
-			}
+
+		String title = "速度";
+		String unit = "km";
+		int index = 0;
+		switch (id) {
+		case R.id.llytSpeed:
+			title = "速度";
+			unit = "km";
+			index = 0;
+			break;
+		case R.id.llytRotary:
+			title = "转速";
+			unit = "rpm";
+			index = 1;
+			break;
+		case R.id.llytVoltage:
+			title = "电源电压";
+			unit = "v";
+			index = 2;
+			break;
+		case R.id.llytTemperature:
+			title = "水温";
+			unit = "℃";
+			index = 3;
+			break;
+		case R.id.llytLoad:
+			title = "负荷";
+			unit = "%";
+			index = 4;
+			break;
+		case R.id.llytThrottle:
+			title = "节气门";
+			unit = "%";
+			index = 5;
+			break;
+		case R.id.llytOil:
+			title = "剩余油量";
+			unit = "%";
+			index = 6;
+			break;
 		}
+		// 先设置单位
+		textUnit.setText(unit);
+		tvCardTitle.setText(title);
+		dialSpeed.startAnimation(value[index], handler);
+		ivCardIcon.setImageResource(icon[index]);
 	}
 
 }
