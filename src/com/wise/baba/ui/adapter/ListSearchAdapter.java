a294479f7b8b9c167d48b72d82add1009d8fc9d7
c <c@c-PC>
2015-06-05 13:56:51 +0800

    加入greendao

--- src/com/wise/baba/ui/adapter/ListSearchAdapter.java
+++ src/com/wise/baba/ui/adapter/ListSearchAdapter.java
@@ -3,13 +3,20 @@
  */
 package com.wise.baba.ui.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.wise.baba.R;
+import com.wise.baba.entity.Suggestion;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  *
@@ -22,6 +29,7 @@ public class ListSearchAdapter extends BaseAdapter {
 
 	
 	private Context context;
+	private List<Suggestion> searchList = new ArrayList<Suggestion>();
 	
 	public ListSearchAdapter(Context context) {
 		super();
@@ -31,12 +39,12 @@ public class ListSearchAdapter extends BaseAdapter {
 	
 	@Override
 	public int getCount() {
-		return 5;
+		return searchList.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
-		return null;
+		return searchList.get(position);
 	}
 	@Override
 	public long getItemId(int position) {
@@ -50,7 +58,44 @@ public class ListSearchAdapter extends BaseAdapter {
 			convertView = LayoutInflater.from(context).inflate(
 					R.layout.item_list_map_search, null);
 		}
+		
+		ImageView ivSearch = (ImageView) convertView.findViewById(R.id.iv_search);
+		TextView tvKey = (TextView) convertView.findViewById(R.id.tv_key);
+		TextView tvCity = (TextView) convertView.findViewById(R.id.tv_city);
+		
+		TextView tvOneLine = (TextView) convertView.findViewById(R.id.tv_one_line);
+		
+		Suggestion  suggestion = searchList.get(position);
+		
+		//历史搜索还是新搜索的
+		if(suggestion.getType() == Suggestion.Type_History){
+			ivSearch.setImageResource(R.drawable.icon_poi_history);
+		}else{
+			ivSearch.setImageResource(R.drawable.icon_search);
+		}
+		
+		tvKey.setText("");
+		//是一行还是二行的内容
+		if(suggestion.getCity()==null || suggestion.getCity().equals("")){
+			tvOneLine.setText(suggestion.getKey());
+			tvKey.setText("");
+			tvCity.setText("");
+		}else{
+			tvKey.setText(suggestion.getKey());
+			tvCity.setText(suggestion.getCity() + suggestion.getDistrict());
+			tvOneLine.setText("");
+		}
+		
+		
 		return convertView;
+	}
+
+
+	/**
+	 * @param searchList
+	 */
+	public void setData(List<Suggestion> searchList) {
+		this.searchList = searchList;
 	}
 
 }
--- src/com/wise/car/SearchLocationActivity.java
+++ src/com/wise/car/SearchLocationActivity.java
@@ -1,44 +1,123 @@
 package com.wise.car;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiNearbySearchOption;
+import com.baidu.mapapi.search.poi.PoiResult;
 import com.baidu.mapapi.search.poi.PoiSearch;
+import com.baidu.mapapi.search.poi.PoiSortType;
+import com.baidu.mapapi.search.sug.OnGetSuggestionResultListener;
+import com.baidu.mapapi.search.sug.SuggestionResult;
+import com.baidu.mapapi.search.sug.SuggestionResult.SuggestionInfo;
 import com.baidu.mapapi.search.sug.SuggestionSearch;
+import com.baidu.mapapi.search.sug.SuggestionSearchOption;
 import com.wise.baba.AppApplication;
 import com.wise.baba.R;
+import com.wise.baba.entity.Suggestion;
 import com.wise.baba.ui.adapter.ListSearchAdapter;
 
-public class SearchLocationActivity extends Activity {
+
+/**
+ * 
+ *
+ * @author c
+ * @desc   百度地图搜索
+ * @date   2015-6-4
+ *
+ */
+public class SearchLocationActivity extends Activity implements TextWatcher, OnGetPoiSearchResultListener, OnGetSuggestionResultListener, OnItemClickListener {
 	private AppApplication app;
 	private PoiSearch mPoiSearch = null;
 	private SuggestionSearch sugSearch = null;
-	private ListView listSearch;
+	private ListView lvSearch;
 	private ListSearchAdapter listSearchAdapter;
+	private EditText etSearch = null;
+	
+	private List<Suggestion> searchList = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_search_location);
 		app = (AppApplication) getApplication();
+		init();
+	}
+	
+	
+	/**
+	 * 初始化
+	 */
+	public void init(){
+		/*
+		 * 先初始化百度搜索
+		 */
 		mPoiSearch = PoiSearch.newInstance();
 		sugSearch = SuggestionSearch.newInstance();
-		mPoiSearch.setOnGetPoiSearchResultListener(null);
-		sugSearch.setOnGetSuggestionResultListener(null);
-		initView();
-	}
-	
-	
-	public void initView(){
-		listSearch = (ListView) findViewById(R.id.lv_search);
+		mPoiSearch.setOnGetPoiSearchResultListener(this);
+		sugSearch.setOnGetSuggestionResultListener(this);
+		
+		
+		/*
+		 * 再初始化界面控件
+		 */
+		searchList = new ArrayList<Suggestion>();
+		lvSearch = (ListView) findViewById(R.id.lv_search);
+		etSearch = (EditText) findViewById(R.id.et_search);
+		
+		etSearch.addTextChangedListener(this);
+		
 		listSearchAdapter= new ListSearchAdapter(this);
-		listSearch.setAdapter(listSearchAdapter);
+		lvSearch.setAdapter(listSearchAdapter);
 		listSearchAdapter.notifyDataSetChanged();
+		lvSearch.setOnItemClickListener(this);
+		
+		
+		findViewById(R.id.iv_back).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				SearchLocationActivity.this.finish();
+			}
+		});
 		
 	}
+	
+	
+	/**
+	 * 
+	 */
+	public void notifyDataSetChanged(){
+		listSearchAdapter.setData(searchList);
+		listSearchAdapter.notifyDataSetChanged();
+	}
+	
+	/**
+	 * @desc  跳到地图界面，显示兴趣点
+	 * @param history_lat
+	 * @param history_lon
+	 * @param re_name
+	 */
 	private void intentToMap(double history_lat, double history_lon,
 			String re_name) {
 		Intent intent = new Intent(this, CarLocationActivity.class);
@@ -48,6 +127,115 @@ public class SearchLocationActivity extends Activity {
 		intent.putExtra("isHotLocation", true);
 		startActivity(intent);
 		this.finish();
+	}
+
+	
+	/**
+	 * 文本框监听
+	 */
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+	}
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+	}
+	@Override
+	public void afterTextChanged(Editable s) {
+		String text = s.toString();
+		SuggestionSearchOption sugOption = new SuggestionSearchOption();
+		sugOption.keyword(text);
+		sugOption.city(app.City);
+		sugSearch.requestSuggestion(sugOption);
+		
+	}
+	
+
+	/**
+	 * POI搜索返回
+	 */
+	@Override
+	public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+		
+	}
+
+	@Override
+	public void onGetPoiResult(PoiResult poiResult) {
+		
+		List<PoiInfo> poiList =  poiResult.getAllPoi();
+		if(poiList == null || poiList.size()==0){
+			Toast.makeText(this, "找不到结果", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		
+		PoiInfo poi =  poiList.get(0);
+		intentToMap(poi.location.latitude,poi.location.longitude,poi.name);
+		
+	}
+
+	
+
+	
+	
+	/**
+	 * 搜索返回建议
+	 */
+	@Override
+	public void onGetSuggestionResult(SuggestionResult suggestionResult) {
+		List<SuggestionInfo> allSuggestions = suggestionResult.getAllSuggestions();
+		if(allSuggestions == null || allSuggestions.size()==0){
+			Toast.makeText(this, "找不到结果", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		
+		this.searchList.clear();
+		Iterator it = allSuggestions.iterator();
+		while(it.hasNext()){
+			SuggestionInfo info = (SuggestionInfo) it.next();
+			Suggestion suggestion = new Suggestion();
+			suggestion.setType(Suggestion.Type_Suggestion);
+			suggestion.setKey(info.key);
+			suggestion.setCity(info.city);
+			suggestion.setDistrict(info.district);
+			
+			
+			searchList.add(suggestion);
+		}
+		notifyDataSetChanged();
+		
+	}
+
+
+	/**
+	 * 点击搜索列表
+	 */
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		Suggestion suggestion = (Suggestion) listSearchAdapter.getItem(position);
+		
+		String city = suggestion.getCity();
+		
+		
+		//去别的城市搜索
+		if(city != null && !city.equals("") && !city.equals(app.City)){
+			PoiCitySearchOption  poiOption  = new PoiCitySearchOption();
+			poiOption.keyword(suggestion.getKey());
+			poiOption.city(city);
+			poiOption.pageNum(0);
+			mPoiSearch.searchInCity(poiOption);
+			return;
+		}
+		
+		
+		
+		//调用附近搜索
+		PoiNearbySearchOption nearByOption = new PoiNearbySearchOption();
+		nearByOption.keyword(suggestion.getKey());
+		nearByOption.location(new LatLng(app.Lat,app.Lon));
+		nearByOption.sortType(PoiSortType.distance_from_near_to_far);
+		nearByOption.radius(1000000000);
+		mPoiSearch.searchNearby(nearByOption);
 	}
 
 
